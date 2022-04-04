0325d86c322f55e5e982b19f4e1f892bebd18d1b
vicky5124 <vickyf5124@gmail.com>
2022-04-04 22:30:30 +0200

    Fixed tasks without eval 	modified:   src/plugins/notifications.py

--- src/plugins/notifications.py
+++ src/plugins/notifications.py
@@ -1,7 +1,10 @@
+import functools
 import logging
-import hikari
 
-from src import utils, main
+import hikari
+from lightbulb.ext import tasks
+
+from src import utils, main, config
 from src.plugins.eval import eval_python
 
 plugin = utils.Plugin("Notifications")
@@ -9,22 +12,34 @@ plugin = utils.Plugin("Notifications")
 
 @plugin.listener(hikari.StartedEvent, bind=True)
 async def start_all_tasks(plug: utils.Plugin, _: hikari.StartedEvent) -> None:
+    async def cron_task(item: config.ConfigNotifications) -> None:
+        logging.debug(item.message)
+        await plug.bot.rest.create_message(item.channel_id, item.message)
+
     for i in plug.bot.config.notifications:
-        logging.info(i)
+        logging.info(f"Starting task for:\n{i.message}")
 
-        await eval_python(
-            plug.bot,
-            f"""
-            from lightbulb.ext import tasks
+        func = functools.partial(cron_task, i)
 
-            @tasks.task(tasks.CronTrigger("{i.cron}"), auto_start=True, wait_before_execution=True)
-            async def cron_task() -> None:
-                logging.warning("{i.message}")
-                await bot.rest.create_message({i.channel_id}, "{i.message}")
+        tsk = tasks.task(tasks.CronTrigger(i.cron), wait_before_execution=True)(func)
+        tsk.start()
 
-            cron_task.start()
-            """,
-        )
+    # for i in plug.bot.config.notifications:
+    #    logging.info(i)
+
+    #    await eval_python(
+    #        plug.bot,
+    #        f"""
+    #        from lightbulb.ext import tasks
+
+    #        @tasks.task(tasks.CronTrigger("{i.cron}"), wait_before_execution=True)
+    #        async def cron_task() -> None:
+    #            logging.warning("{i.message}")
+    #            await bot.rest.create_message({i.channel_id}, "{i.message}")
+
+    #        cron_task.start()
+    #        """,
+    #    )
 
 
 def load(bot: main.CiberBot) -> None:
