171e746d25a3a56e1da8607122f4f3e17c5385c6
AmyMagdalena <98350291+AmyMagdalena@users.noreply.github.com>
2023-05-22 23:30:41 +0200

    added comments and a couple more punctuation markers

--- src/uwuri_parser.py
+++ src/uwuri_parser.py
@@ -3,17 +3,23 @@ import typing as t
 
 
 def uwuriparser(link: str) -> t.List[str]:
+    # Initialise Empty Variables
     urls = []
     words = link.split()
     posa = 0
     posb = 0
     for word in words:
+        # Check if last character is punctuation unlikely to be found as the last character in a URI
         if word[len(word) - 1] in """!$&(*,;""":
             posb += 1
-        if (word[0] in """(['"{""") and (word[len(word) - (posb + 1)] in """)]'"}"""):
+        # Checks if the first character is an opening bracket or quotation mark, then checks if the last is too
+        if (word[0] in """(['‘"“{"""):
+            if (word[len(word) - (posb + 1)] in """)]'’"”}"""):
+                posb += 1
             posa += 1
-            posb += 1
+        # Runs word against python's built-in URI checker
         parsed = urlparse(word[posa:-posb])
+        # If URI, Appends to List
         if parsed.netloc:
             urls.append(word)
     return urls
