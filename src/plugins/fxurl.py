4fd9368d6a153d7e0eaf7674ffd65a4fd7e90c42
AmyMagdalena <98350291+AmyMagdalena@users.noreply.github.com>
2023-05-22 08:52:52 +0200

    Black Sourced

--- src/plugins/fxurl.py
+++ src/plugins/fxurl.py
@@ -10,9 +10,10 @@ from src import utils, main
 plugin = utils.Plugin("FX URLs")
 
 
-#URL_REGEX = r"""((?:(?:https|ftp|http)?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|org|es|cat|net)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|uk|ac)\b/?(?!@)))"""
-#Simplified Regex to only care about .com and .net and http and https, also escaped /
+# URL_REGEX = r"""((?:(?:https|ftp|http)?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|org|es|cat|net)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|uk|ac)\b/?(?!@)))"""
+# Simplified Regex to only care about .com and .net and http and https, also escaped /
 URL_REGEX = r"""((?:(?:https|http)?:(?:\/{1,3}|[\w%])|[\w.\-]+[.](?:com|net)\/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[\w]+(?:[.\-][a-z0-9]+)*[.](?:com|net)\b\/?(?!@)))"""
+
 
 @plugin.listener(hikari.MessageCreateEvent)  # type: ignore
 async def on_message(event: hikari.MessageCreateEvent) -> None:
@@ -28,15 +29,15 @@ async def on_message(event: hikari.MessageCreateEvent) -> None:
     if not urls:
         return
 
-    fx_url= ""
+    fx_url = ""
 
     for idx, i in enumerate(urls):
         url = urlparse(i)
         if url.hostname in {"twitter.com", "www.twitter.com", "mobile.twitter.com"}:
-            #msg_to_send += f"[Link {idx}](https://fxtwitter.com{url.path}) "
+            # msg_to_send += f"[Link {idx}](https://fxtwitter.com{url.path}) "
             fx_url += f"https://fxtwitter.com{url.path}\n"
         if url.hostname in {"pixiv.net", "www.pixiv.net"}:
-            #msg_to_send += f"[Link {idx}](https://fxpixiv.net{url.path}) "
+            # msg_to_send += f"[Link {idx}](https://fxpixiv.net{url.path}) "
             fx_url += f"https://fxpixiv.net{url.path}\n"
 
     # If the message had URLs that werent twitter or pixiv, skip the fixing.
